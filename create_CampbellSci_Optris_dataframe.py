a4a5c488fd12c9608752e5fd8a66223538fe10d9
kplo373 <115310147+kplo373@users.noreply.github.com>
2024-09-17 20:52:44 +1200

    update to creating merged df!
    Fixed the NaNs error when merging the dataframes, now just need to make sure it also includes the standard deviation and standard error in merged_df.

--- create_CampbellSci_Optris_dataframe.py
+++ create_CampbellSci_Optris_dataframe.py
@@ -12,58 +12,35 @@ common time data should be included in the dataframe.
 import pandas as pd
 #import numpy as np
 
-# copy next bit from plot_hot_cold_retry.py into here and probably read in previous functions... see test for resample_Optris.py
-
-#resample_Optris outputs resampled_df - an Optris dataframe, will probably need this
-# where does the CampbellSci_df come from? We get them separately from read_CampbellSci.py and its averaging functions below...should I put these into a df?
 def create_CampbellSci_Optris_dataframe(Optris_df, CampbellSci_df):
-
+    print(Optris_df.columns)  # make sure to insert the standard deviation and standard error arrays per sensor into the end df_merged!
+    print(CampbellSci_df.columns)
+    
     # Setting up dataframes for Optris and Campbell Scientific cold temperature arrays
-    dt_Op = pd.to_datetime(Optris_df.index)  # getting datetime index column as an array from Optris, then converting to pandas datetime
     mean_Op = Optris_df['Op_temp']
     datetime_Op = Optris_df['datetimes']
     T_Op = mean_Op.to_numpy()  # creating a numpy array for the Optris average temperatures
-    #print(T_Op)  # also legit
-    df_optris = pd.DataFrame({'temperature': T_Op}, index = datetime_Op)  # creating a new dataframe for Optris
-    #print(df_optris['temperature'])  # has worked here!
-    
-    # Resample to 5-second intervals, taking the mean for each interval - why am I doing this again actually?? Optris_df should already be in 5s intervals...
-    #df_optris_resampled = df_optris.resample('5s').mean()
-    
-    # Extract the resampled temperature and time arrays
-    #Tcold_Op_resampled = df_optris_resampled['temperature'].values
-    #Tcold_time_Op_resampled = df_optris_resampled.index
+    df_optris = pd.DataFrame({'temperature_Op': T_Op}, index = datetime_Op)  # creating a new dataframe for Optris
     
     dt_objCS = CampbellSci_df['datetimes']
-    #print(dt_objCS)  # good
-    T_CS = CampbellSci_df['mean_temperatures']
-    #print(T_CS)  # this is legit
+    T_CS = CampbellSci_df['mean_temperatures'].values  # adding .values converts it from pd.series to np.ndarray and prevents NaNs in final dataframe
     
     # Verify the lengths now match
     print(f"Length of T_Op: {len(T_Op)}")
     print(f"Length of T_CS: {len(T_CS)}")  # they don't actually match, but are on the same scale now! df_merged line below only includes values from both arrays
     
     # Create a dataframe for C1 data
-    df_CS = pd.DataFrame({'temperatures': T_CS}, index = dt_objCS)
-    #print(df_CS.index)  # good
-    print(len(dt_objCS), len(df_CS['temperatures']))  # both are length 5061
-    print(df_CS['temperatures'])  # this is not legit... is all NaNs. Why? T_CS as an array is normal!
+    df_CS = pd.DataFrame({'temperature_CS': T_CS}, index = dt_objCS)
     
     # Merge the resampled Optris data with the C1 data
     df_merged = df_CS.join(df_optris, how='inner', lsuffix='_CS', rsuffix='_Op')
     
-    # Now you can access the aligned temperature arrays
-    #Tcold_C1_aligned = df_merged['temperature_C1'].values
-   # Tcold_Op_aligned = df_merged['temperature_Op'].values
-    
     return df_merged
 
 df_merged = create_CampbellSci_Optris_dataframe(avgOp_df, df_sand_avgCS)
-#print(df_merged)
+print(df_merged)
 
-
-#%% Test function
-#what would I need to get from past functions?? this should follow on from resample_Optris.py
+#%% Required functions to test the function above
 
 # To get the filepath
 from get_filepaths import get_filepaths
@@ -94,7 +71,6 @@ from read_Optris import average_Optris
 avgOp_df = average_Optris(resampled_df_a1, resampled_df_a3)
 # print(avgOp_df)
 
-#%% test this function
-
+#%% Test this actual function
 df_merged = create_CampbellSci_Optris_dataframe(avgOp_df, df_sand_avgCS)
 print(df_merged)
